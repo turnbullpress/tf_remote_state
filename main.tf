4378412dfabba750af01e0e5dd5adcfe97c7acb6
James Turnbull <james@lovedthanlost.net>
2019-06-27 10:21:23 -0400

    Added lifecycle

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "remote_state" {
-  bucket = "${var.prefix}-remote-state-${var.environment}"
-  acl    = "authenticated-read"
-
+  bucket        = "${var.prefix}-remote-state-${var.environment}"
+  acl           = "authenticated-read"
+  force_destroy = false
   # Never want to delete your state bucket
+
   lifecycle {
     prevent_destroy = true
   }
